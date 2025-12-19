04ea5390d9a13adb8b4df34d40a7977c98513d23
Nguyễn Minh Lộc <Nguyn Minh Lc@DESKTOP-MINHLOC>
2025-12-19 21:28:53 +0700

    Hoàn thiện module quản lý ngôn ngữ và quản lý vị trí

--- UTT.Library.BLL/Services/BLL_ViTri.cs
+++ UTT.Library.BLL/Services/BLL_ViTri.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UTT.Library.DAL.Repositories;
+using UTT.Library.DTO;
 
 namespace UTT.Library.BLL.Services
 {
-    internal class BLL_ViTri
+    public class BLL_ViTri
     {
+        private DAL_ViTri _dal = new DAL_ViTri();
+
+        public DataTable LayDanhSach()
+        {
+            return _dal.GetDanhSach();
+        }
+
+        public DataTable TimKiem(string keyword)
+        {
+            return _dal.TimKiem(keyword);
+        }
+
+        public string Them(DTO_ViTri vt)
+        {
+            if (_dal.Them(vt)) return ""; 
+            return "Không thể thêm vị trí kệ (có thể trùng mã)!";
+        }
+
+        public string Sua(DTO_ViTri vt)
+        {
+            if (_dal.Sua(vt)) return "";
+            return "Cập nhật thất bại!";
+        }
+
+        public string Xoa(string ma)
+        {
+            if (_dal.Xoa(ma)) return "";
+            return "Xóa thất bại (Dữ liệu đang được sử dụng)!";
+        }
     }
 }
--- UTT.Library.DAL/Repositories/DAL_KhoSach.cs
+++ UTT.Library.DAL/Repositories/DAL_KhoSach.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UTT.Library.DAL.Database;
+using System.Data;
 namespace UTT.Library.DAL.Repositories
 {
-    internal class DAL_KhoSach
+    public class DAL_KhoSach
     {
+
+        private DatabaseHelper _db = new DatabaseHelper();
+
+        public DataTable GetDanhSach()
+        {
+            return _db.GetDataTable("SELECT MaKho, TenKho FROM KHOSACH");
+        }
     }
 }
